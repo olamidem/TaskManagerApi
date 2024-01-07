acf8e66c3284cc9d07fa375ff2afdd467795c3a8
Olamide <olamxii@gmail.com>
2024-01-07 16:31:53 +0100

    Task ManagerAPi first commit

---
+++ app/Http/Controllers/TaskController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Resources\TaskCollection;
+use App\Http\Resources\TaskResource;
+use App\Models\Task;
+use Illuminate\Http\Request;
+
+class TaskController extends Controller
+{
+
+    public function index(Request $request)
+    {
+        return new TaskCollection(Task::all());
+    }
+
+    public function show(Request $request, Task $task)
+    {
+        return new TaskResource($task);
+    }
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'title' => 'required|max:255'
+        ]);
+        $task = Task::create($validated);
+        return new TaskResource($task);
+    }
+}
---
+++ app/Models/Task.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+    ];
+
+    protected $casts = [
+        'is_done' => 'boolean'
+    ];
+
+    protected $hidden = [
+        'updated_at'
+    ];
+}
